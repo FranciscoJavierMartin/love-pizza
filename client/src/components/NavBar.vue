becc166a9f90be20b96796d77c292a21558f935b
Francisco <javiermartincuenca@gmail.com>
2021-01-28 20:44:25 +0100

    Added routes to navbar

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>
-    <div>Love Pizza</div>
+  <nav>
+    <router-link :to="HOME_PAGE_ROUTE">Love Pizza</router-link>
     <div>
-      <button>Login</button>
-      <button>Register</button>
+      <router-link :to="LOGIN_PAGE_ROUTE">Login</router-link>
+      <router-link :to="REGISTER_PAGE_ROUTE">Register</router-link>
     </div>
-  </div>
+  </nav>
 </template>
+
+<script lang="ts">
+import {
+  HOME_PAGE_ROUTE,
+  REGISTER_PAGE_ROUTE,
+  LOGIN_PAGE_ROUTE,
+} from '../constants/routes';
+
+export default {
+  setup() {
+    return { HOME_PAGE_ROUTE, REGISTER_PAGE_ROUTE, LOGIN_PAGE_ROUTE };
+  },
+};
+</script>
