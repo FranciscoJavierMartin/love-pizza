64e2d24f2e49afcd3011237aa84692e8eddc98fe
Francisco <javiermartincuenca@gmail.com>
2021-01-28 20:25:42 +0100

    Added prettier

---
+++ client/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  tabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  printWidth: 80,
+};
--- client/src/App.vue
+++ client/src/App.vue
@@ -6,8 +6,8 @@
 </template>
 
 <script  lang="ts">
-import { ref, defineComponent } from "vue";
-import NavBar from "./components/NavBar.vue";
+import { ref, defineComponent } from 'vue';
+import NavBar from './components/NavBar.vue';
 
 export default defineComponent({
   components: {
