0df48087d88b43f96571fb257d632c517f14f84b
Francisco <javiermartincuenca@gmail.com>
2021-01-28 20:14:44 +0100

    Added navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,4 +1,17 @@
 <template>
-  <h1>Hello world</h1>
+  <div>
+    <nav-bar />
+    <h1>Hello world</h1>
+  </div>
 </template>
 
+<script  lang="ts">
+import { ref, defineComponent } from "vue";
+import NavBar from "./components/NavBar.vue";
+
+export default defineComponent({
+  components: {
+    NavBar,
+  },
+});
+</script>
---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div>Love Pizza</div>
+    <div>
+      <button>Login</button>
+      <button>Register</button>
+    </div>
+  </div>
+</template>
