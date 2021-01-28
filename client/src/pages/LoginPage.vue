2e33b433db54bba0933627e93bb480ec3025fc00
Francisco <javiermartincuenca@gmail.com>
2021-01-28 23:24:12 +0100

    Login form added

--- client/src/pages/LoginPage.vue
+++ client/src/pages/LoginPage.vue
@@ -1,3 +1,36 @@
 <template>
-  <h1>Login</h1>
+  <div>
+    <form @submit.prevent="loginUser">
+      <div>
+        <input placeholder="Name" type="text" v-model="username" />
+      </div>
+      <div>
+        <input placeholder="Password" type="password" v-model="password" />
+      </div>
+      <div>
+        <button type="submit" @click.prevent="loginUser">Login</button>
+      </div>
+    </form>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const username = ref<string>('');
+    const password = ref<string>('');
+
+    const loginUser = () => {
+      console.log('Username', username.value, 'Password', password.value);
+    };
+
+    return {
+      username,
+      password,
+      loginUser,
+    };
+  },
+});
+</script>
