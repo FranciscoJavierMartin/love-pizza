1e55a276e8a6bb5130b39f33c8ac3f2d211e88bc
Francisco <javiermartincuenca@gmail.com>
2021-01-28 23:26:57 +0100

    Register form added

--- client/src/pages/RegisterPage.vue
+++ client/src/pages/RegisterPage.vue
@@ -1,3 +1,41 @@
 <template>
-  <h1>Register</h1>
+  <div>
+    <form @submit.prevent="registerUser">
+      <div>
+        <input placeholder="Name" type="text" v-model="username" />
+      </div>
+      <div>
+        <input placeholder="Password" type="password" v-model="password" />
+      </div>
+      <div>
+        <input placeholder="Confirm Password" type="password" v-model="confirmPassword" />
+      </div>
+      <div>
+        <button type="submit" @click.prevent="registerUser">Register</button>
+      </div>
+    </form>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const username = ref<string>('');
+    const password = ref<string>('');
+    const confirmPassword = ref<string>('');
+
+    const registerUser = () => {
+      console.log('Username', username.value, 'Password', password.value);
+    };
+
+    return {
+      username,
+      password,
+      confirmPassword,
+      registerUser,
+    };
+  },
+});
+</script>
