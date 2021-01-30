59953f77806dcaf6fc3e2532508f31cc8bc83558
Francisco <javiermartincuenca@gmail.com>
2021-01-30 20:14:41 +0100

    Added enpoints login and register

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.DTO;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+  public class UsersController : BaseApiController
+  {
+    private readonly DataContext _context;
+    private readonly ITokenService _tokenService;
+    public UsersController(DataContext context, ITokenService tokenService)
+    {
+      _tokenService = tokenService;
+      _context = context;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+    {
+      ActionResult<UserDto> res;
+      if (await UserExists(registerDto.Username))
+      {
+        res = BadRequest("Username is taken");
+      }
+      else
+      {
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+          UserName = registerDto.Username.ToLower(),
+          PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+          PasswordSalt = hmac.Key
+        };
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        res = new UserDto
+        {
+          Username = user.UserName,
+          Token = _tokenService.CreateToken(user)
+        };
+      }
+      return res;
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+      ActionResult<UserDto> res;
+
+      var user = await _context.Users
+        .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+      if (user == null)
+      {
+        res = Unauthorized("Invalid username");
+      }
+      else
+      {
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+          if (computedHash[i] != user.PasswordHash[i])
+          {
+            return Unauthorized("Invalid password");
+          }
+        }
+
+        res = new UserDto
+        {
+          Username = user.UserName,
+          Token = _tokenService.CreateToken(user)
+        };
+      }
+
+      return res;
+    }
+
+    private async Task<bool> UserExists(string username)
+    {
+      return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+    }
+
+  }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +12,21 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+
+            try {
+
+            } catch(Exception ex){
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred during migration");
+            }
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
