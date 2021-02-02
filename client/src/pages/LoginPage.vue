21df4f32e0f6f7bd026cfd13d6e3f93c02f80eb5
Francisco <javiermartincuenca@gmail.com>
2021-02-02 11:46:06 +0100

    Login form styled

--- client/src/pages/LoginPage.vue
+++ client/src/pages/LoginPage.vue
@@ -1,14 +1,35 @@
 <template>
   <div>
+    <h1 class="text-center">Login</h1>
     <form @submit.prevent="loginUser">
-      <div>
-        <input placeholder="Name" type="text" v-model="username" />
+      <div class="form-group">
+        <label for="inputEmail">User name</label>
+        <input
+          id="inputEmail"
+          placeholder="User name"
+          class="form-control"
+          type="text"
+          v-model="username"
+        />
+      </div>
+      <div class="form-group">
+        <label for="inputPassword">Email address</label>
+        <input
+          id="inputPassword"
+          placeholder="Password"
+          class="form-control"
+          type="password"
+          v-model="password"
+        />
       </div>
       <div>
-        <input placeholder="Password" type="password" v-model="password" />
-      </div>
-      <div>
-        <button type="submit" @click.prevent="loginUser">Login</button>
+        <button
+          type="submit"
+          class="btn btn-primary btn-lg btn-block"
+          @click.prevent="loginUser"
+        >
+          Login
+        </button>
       </div>
     </form>
   </div>
